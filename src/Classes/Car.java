eeec9f59d73aafc5eb913818e633dec474ae87e4
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-02-21 15:58:08 -0600

    Completed Classes

--- src/Classes/Car.java
+++ src/Classes/Car.java
@@ -1,4 +1,27 @@
 package Classes;
 
 public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    // Create method to update the model.
+    public void setModel(String model) {
+        // Use "this" when referring to field of the class.
+        // this.model = model;
+        String validModel = model.toLowerCase();
+        if (validModel.equals("civic") || validModel.equals("tundra")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    // Create the getter
+    public String getModel() {
+        return this.model;
+    }
 }
--- src/Classes/ClassesExercise.java
+++ src/Classes/ClassesExercise.java
@@ -2,6 +2,15 @@ package Classes;
 
 public class ClassesExercise {
     public static void main(String[] args) {
+        // Creating the object
+        Car honda = new Car();
+        Car toyota = new Car();
 
+        // Call the set.model from car.
+        honda.setModel("Civic");
+        System.out.println("Model is " + honda.getModel());
+
+        honda.setModel("Accord");
+        System.out.println("Model is " + honda.getModel());
     }
 }
